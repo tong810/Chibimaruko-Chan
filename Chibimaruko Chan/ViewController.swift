fb623c84feed569413266bfbbced2173a30a54b3
tong <zz472780@gmail.com>
2024-09-04 21:07:11 +0800

    Initial Commit

---
+++ Chibimaruko Chan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chibimaruko Chan
+//
+//  Created by 陳曉潼 on 2024/9/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
